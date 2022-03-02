611cb3201e288bf88a39003aeeec75b1a2ed8ebf
Nora <norahanegan@gmail.com>
2022-03-02 12:58:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 client_id = '948366267725795328'
 RPC = Presence(client_id)
 RPC.connect()
-
+print("Successfully Connected to Discord")
 RPC.update(state="https://war.ukraine.ua/", details="#StopRussianAggression", large_image="flag_of_ukraine_svg", large_text="Ukranian Flag", buttons=[{"label": "#StandWithUkraine", "url": "https://war.ukraine.ua/stand-with-ukraine/"}, {"label": "Why?", "url": "https://war.ukraine.ua/why/"}])
 
 while True:
